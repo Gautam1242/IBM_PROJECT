9188f5aa4443ff5c65aaadd457803a8e7fd67eab
Gautam Shah <gautamshah317@gmail.com>
2024-07-14 18:26:18 +0400

    footer part is done

--- js/main.js
+++ js/main.js
@@ -14,3 +14,22 @@ cards.forEach(card => {
         card.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
     });
 });
+
+// JavaScript for scroll-to-top button
+const scrollTopBtn = document.querySelector("#scrollTopBtn");
+
+window.addEventListener("scroll", function () {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+});
+
+scrollTopBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
